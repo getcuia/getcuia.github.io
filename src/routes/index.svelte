da0e2f3b63ec65091c0a5891654ee18b11174d14
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-20 16:31:21 -0300

    Improve index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,10 @@
-<script lang="ts">
+<script lang="ts" context="module">
     import Readme from "../Readme.svelte";
+
+    export const prerender = true;
+
+    const url =
+        "https://raw.githubusercontent.com/getcuia/getcuia.github.io/main/README.md";
 </script>
 
-<Readme
-    url="https://raw.githubusercontent.com/getcuia/getcuia.github.io/main/README.md"
-/>
+<Readme {url} />

2a6f0ed39727864c3651d69b2b0ca7427ccde04f
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-20 20:51:00 -0300

    Prepare a webpage for other subprojects

---
+++ src/routes/p/[name].svelte
@@ -0,0 +1,8 @@
+<script lang="ts" context="module">
+    import Project from "../../Project.svelte";
+    export const prerender = true;
+
+    import { page } from "$app/stores";
+</script>
+
+<Project name={$page.params.name} />

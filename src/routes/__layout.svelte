8dc8ab1a48ee1ccbec633ffc55b83603516f85e9
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-20 20:17:48 -0300

    Control size of logo

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,4 +6,14 @@
 
 <style>
     @import url("https://gitcdn.link/cdn/dohliam/dropin-minimal-css/gh-pages/min/github-markdown.min.css");
+
+    :global(.logo) {
+        margin: 4em 0;
+    }
+
+    @media (max-width: 480px) {
+        :global(.logo) {
+            width: 50%;
+        }
+    }
 </style>

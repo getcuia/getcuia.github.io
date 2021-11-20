d3c9f515e4c306b9435efeff5050dd54b90fbe4e
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-20 20:50:21 -0300

    Reuse stuff in Project.svelte

---
+++ src/Project.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import Readme from "./Readme.svelte";
+
+    export let name = "getcuia.github.io";
+    export let title = "cuia 🌿";
+    export let description = "We help make console user interfaces awesome 🌿";
+
+    const url = `https://gitcdn.link/cdn/getcuia/${name}/main/README.md`;
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+    <meta name="description" content={description} />
+</svelte:head>
+
+<Readme {url} />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,6 @@
 <script lang="ts" context="module">
-    import Readme from "../Readme.svelte";
-
+    import Project from "../Project.svelte";
     export const prerender = true;
-
-    const url =
-        "https://gitcdn.link/cdn/getcuia/getcuia.github.io/main/README.md";
 </script>
 
-<svelte:head>
-    <title>cuia</title>
-    <meta
-        name="description"
-        content="We help make console user interfaces awesome 🌿"
-    />
-</svelte:head>
-
-<Readme {url} />
+<Project />
